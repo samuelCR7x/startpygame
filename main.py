372dbf56652fe45066d22f27ad627dc65d090dec
samuelCR7x <cristhiansamuel00@gmail.com>
2025-04-28 12:31:21 -0500

    velocidad y color

--- main.py
+++ main.py
@@ -13,9 +13,37 @@ ventana = pygame.display.set_mode((ancho, alto))
 
 pygame.display.set_caption("mi primer juego")
 
+"""variables de movimiento"""
+mover_arriba = False 
+mover_abajo = False
+mover_izquierda = False
+mover_derecha = False
+
+reloj = pygame.time.clock()
+
 run = True
 
 while run:
+
+    reloj.tick(constantes.FPS)
+
+    ventana.fill(constantes.COLOR_BG)
+
+    """calcular el movimiento"""
+    delta_x = 0
+    delta_y = 0
+
+    if mover_derecha == True:
+        delta_x = constantes.VELOCIDAD
+    if mover_izquierda == True:
+        delta_x = -constantes.VELOCIDAD
+    if mover_arriba == True:
+        delta_y = -constantes.VELOCIDAD
+    if mover_abajo == True:
+        delta_y = constantes.VELOCIDAD
+
+    player.movimiento(delta_x, delta_y)
+
 
 
     player.dibujar(ventana)
@@ -23,15 +51,26 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_a:
-                print("izquierda")
+                mover_izquierda = True
             if event.key == pygame.k_d:
-                print("derecha")
+                mover_derecha = True
             if event.key == pygame.K_w:
-                print("arriba")
+                mover_arriba = True
             if event.key == pygame.k_s:
-                print("abajo")
+                mover_abajo = True
+        
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                mover_izquierda = False
+            if event.key == pygame.k_d:
+                mover_derecha = False
+            if event.key == pygame.K_w:
+                mover_arriba = False
+            if event.key == pygame.k_s:
+                mover_abajo = False
 
     pygame.display.uptade() 
 
